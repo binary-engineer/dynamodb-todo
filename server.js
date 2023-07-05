bef8e4facbad9b997a67755384b17c0b3779b6ff
obi-wan <cyberkid042@outlook.com>
2023-07-05 13:11:43 -0300

    Minor update

--- server.js
+++ server.js
@@ -2,13 +2,27 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 
-const dynamodb = require("@cyclic.sh/dynamodb")({
-  accessKeyId: "YOUR_ACCESS_KEY",
-  secretAccessKey: "YOUR_SECRET_ACCESS_KEY",
-  region: "YOUR_REGION",
-});
+const CyclicDb = require("@cyclic.sh/dynamodb");
+const db = CyclicDb("ill-pink-katydid-cuffCyclicDB");
 
 app.use(express.json());
+
+const animals = db.collection("animals");
+
+// let item = await animals.get("leo");
+// console.log(item);
+
+app.get("/", async (req, res) => {
+  animals
+    .get("leo")
+    .then((result) => {
+      res.json(result.Items);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({ error: "An error occurred" });
+    });
+});
 
 const port = 3000 || process.env.PORT;
 app.listen(port, () => {
